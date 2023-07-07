e3609fb5f289357df6cd2a6fd6a0118c65d94b16
Jean Carlos de Oliveira <jeancarlosdeoliveira@outlook.com>
2023-07-07 14:57:42 -0300

    feat initial commit

---
+++ Extencao PSOffice Agile/backend.js
@@ -0,0 +1,11 @@
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    if (msg.command == "getTimes") {
+
+        var time = [...document.getElementsByClassName("row-list-component")].map(e => { return e.getElementsByClassName("col-md-6")[3].getElementsByClassName("col-md-2")[0].getElementsByClassName("font-weight-bold")[0].innerHTML })
+
+        window.ScraperExt = time;
+        chrome.runtime.sendMessage({ command: "complete", data: window.ScraperExt });
+    } else {
+        chrome.runtime.sendMessage({ command: "uncomplete", data: undefined });
+    }
+})
---
+++ Extencao PSOffice Agile/frontend.js
@@ -0,0 +1,27 @@
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    if (msg.command == 'complete') {
+        let total = moment.duration(0);
+
+        msg.data.forEach(element => {
+            total.add(moment.duration(element))
+        });
+
+        totalFormatado = moment.utc(total.asMilliseconds()).format("HH:mm");
+
+        document.querySelector('.extTotal').innerHTML = totalFormatado;
+    }
+});
+
+
+document.querySelector('.container .btnClickExt').addEventListener('click', function (e) {
+
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var activeTab = tabs[0];
+
+        if (activeTab.url.includes('pso-zenvia.jexperts.cloud/agile/agiles')) {
+            chrome.tabs.sendMessage(activeTab.id, { command: "getTimes", data: [""] });
+        }
+    });
+
+
+});
